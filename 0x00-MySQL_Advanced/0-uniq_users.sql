6d12dbf9524bb18490737f2e64de6294f0ff19de
lilieyen <lnyasita@gmail.com>
2021-08-11 19:53:42 +0300

    create table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,8 @@
 -- create a table users
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255),
+	name VARCHAR(255)
 );
 
