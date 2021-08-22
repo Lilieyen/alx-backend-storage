356329074ca8707f6634a80675f147ecd6b6001b
lilieyen <lnyasita@gmail.com>
2021-08-22 08:44:34 +0300

    provide some stats about nginx

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -15,7 +15,8 @@ def log_stats():
     put = logs_collection.count_documents({"method": "PUT"})
     patch = logs_collection.count_documents({"method": "PATCH"})
     delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents({"method": "GET", "path": "/status"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
     print(f"{total} logs")
     print("Methods:")
     print(f"\tmethod GET: {get}")
@@ -23,7 +24,7 @@ def log_stats():
     print(f"\tmethod PUT: {put}")
     print(f"\tmethod PATCH: {patch}")
     print(f"\tmethod DELETE: {delete}")
-    print(F"{path} status check")
+    print(f"{path} status check")
 
 
 if __name__ == "__main__":
