163bc02db053528f82f4eb5573c402af7c8b2aa0
lilieyen <lnyasita@gmail.com>
2021-08-22 10:20:01 +0300

    provide some stats about nginx

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,30 +1,25 @@
 #!/usr/bin/env python3
 """pyhton script that provides some stats about Nginx logs stored in MongoDB"""
-import pymongo
+from pymongo import MongoClient
 
 
-def log_stats(a: dict) -> int:
+def log_stats():
     """
-    log the statistics & connect to the default host
+    log stats
     """
-    connection = pymongo.MongoClient()
-    logs = connection.logs.nginx
-    return logs.count_documents(a)
+    client = MongoClient()
+    collec_nginx = client.logs.nginx
 
-
-def main_function():
-    """
-    main function
-    """
-    print(f"{log_stats({})} logs")
+    num_of_docs = collec_nginx.count_documents({})
+    print("{} logs".format(num_of_docs))
     print("Methods:")
-    print(f"\tmethod GET: {log_stats({'method': 'GET'})}")
-    print(f"\tmethod POST: {log_stats({'method': 'POST'})}")
-    print(f"\tmethod PUT: {log_stats({'method': 'PUT'})}")
-    print(f"\tmethod PATCH: {log_stats({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {log_stats({'method': 'DELETE'})}")
-    print(f"{log_stats({'method': 'GET', 'path': '/status'})} status check")
+    methods_list = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods_list:
+        method_count = collec_nginx.count_documents({"method": method})
+        print("\tmethod {}: {}".format(method, method_count))
+    status = collec_nginx.count_documents({"method": "GET", "path": "/status"})
+    print("{} status check".format(status))
 
 
 if __name__ == "__main__":
-    main_function()
+    log_stats()
